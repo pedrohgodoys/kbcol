abb2ea70d94f71e92d4cd4377a2c97b602b0bde4
Pedro H. Godoy Sanches <45294360+pedrohgodoys@users.noreply.github.com>
2023-01-01 01:50:47 -0300

    Delete kbcol.sh

--- kbcol.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-model=$(g512-led --print-device | awk '{print tolower($4)}')"-led" # find the keyboard model
-RGB=$(zenity --color-selection --show-palette | sed -e 's/[rgb\(\)]//gm' -e 's/[\,]/ /gm') # colour selection
-[[ ! -z $RGB ]] && $model -a $(printf "%02X%02X%02X" ${RGB}) # colour validation
